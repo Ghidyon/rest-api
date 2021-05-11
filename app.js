f3295a3ae5a55f753596034c7c4cd8b276db7121
ghidyon <gideonakunana@gmail.com>
2021-05-11 18:34:14 +0100

    made post request

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-// * Creating a server
-const app = require('express')();
+const express = require('express');
 
-// * Configure app to listen to a port
-const port = 7070;
-app.listen(port, () => {
-    console.log(`App is running on port:${port}`);
-});
+// * Creating a server
+const app = express();
+
+// * Access users
+const users = require('./users.json');
 
 /* 
 * HTTP Request Methods
@@ -24,4 +23,48 @@ app.get('/', (req, res) => {
 
 app.get('/books', (req, res) => {
     res.send('There are 75 books in store.');
+});
+
+app.post('/', (req, res) => {
+    res.send('Default post request!');
+});
+
+// TODO: REST API using express
+/* 
+    * Request Object
+    - params
+    - query
+    - body
+*/
+/* 
+    * CRUD Operations
+   - Creating users,            POST /users
+   - Fetching users,            GET /users
+   - Fetching a single user,    GET /users/:id
+   - Updating a single user,    PUT /users/:id
+   - Deleting a single user,    DELETE /users/:id
+*/
+
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+app.get('/users', (req, res) => {
+    // fetch all users
+    // send array of users as a response to the client
+    return res.json({ users });
+});
+
+app.post('/users', (req, res) => {
+    // create a new user from client's response
+    console.log(req.body);
+    // save new user to existing database
+    // save updated data to users.json
+    // send back a response to client
+});
+
+
+// * Configure app to listen to a port
+const port = 7070;
+app.listen(port, () => {
+    console.log(`App is running on port:${port}`);
 });
